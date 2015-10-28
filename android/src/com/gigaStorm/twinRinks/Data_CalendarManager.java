7e9fc07779d3f4eb63d29b9709bdbd4c6c50a6db
Boris D <dubi13@gmail.com>
2015-10-27 19:40:11 -0500

    Move the code into Studio (Eclipse is no longer supported)
    - Fix the time zone for calendar entries.
    - Make sure there are no double entries in calendar.

--- android/src/com/gigaStorm/twinRinks/Data_CalendarManager.java
+++ android/src/com/gigaStorm/twinRinks/Data_CalendarManager.java
@@ -57,8 +57,13 @@ public class Data_CalendarManager {
 
     if(result.getCount() > 0 && result.moveToFirst()) {
       do {
-        listCals.add(result.getString(result
-            .getColumnIndex(CalendarContract.Calendars.NAME)));
+    	  String	tmp = result.getString(result
+    	     .getColumnIndex(CalendarContract.Calendars.NAME));
+    	  if (tmp == null)
+    		  tmp = result.getString(result
+    				  .getColumnIndex(
+    						  CalendarContract.Calendars.CALENDAR_DISPLAY_NAME));
+    	  listCals.add(tmp);
       }
       while(result.moveToNext());
     }
@@ -97,19 +102,71 @@ public class Data_CalendarManager {
     ContentResolver cr = context.getContentResolver();
     ContentValues values = new ContentValues();
 
-    try {
-      values.put(CalendarContract.Events.CALENDAR_ID, whichCalendar);
-      values.put(CalendarContract.Events.TITLE, "Hockey- " + game.getLeague()
-          + ": " + game.getTeamH() + " vs " + game.getTeamA());
-      values.put(CalendarContract.Events.EVENT_LOCATION,
-          "Twin Rinks Ice Arena - " + game.getRink() + " Rink");
-      values.put(CalendarContract.Events.DTSTART, game.getCal()
-          .getTimeInMillis());
-      values.put(CalendarContract.Events.DTEND,
-          game.getCal().getTimeInMillis() + 5400000);
-      values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault()
-          .getID());
-      cr.insert(CalendarContract.Events.CONTENT_URI, values);
+    try {   
+      String[] query = new String[] {
+    	  CalendarContract.Events.CALENDAR_ID,
+    	  CalendarContract.Events.TITLE,
+    	  CalendarContract.Events.EVENT_LOCATION,
+    	  CalendarContract.Events.DTSTART,
+    	  CalendarContract.Events.DTEND,
+    	  CalendarContract.Events.EVENT_TIMEZONE
+      };
+      
+      final int PROJECTION_CALENDAR_ID_INDEX = 0;
+      final int PROJECTION_TITLE_INDEX = 1;
+      final int PROJECTION_EVENT_LOCATION_INDEX = 2;
+      final int PROJECTION_DTSTART_INDEX = 3;
+      
+      Cursor cursor = context.getContentResolver().query(
+    		  CalendarContract.Events.CONTENT_URI,
+    		  query, null, null, null);
+      Uri uri = CalendarContract.Calendars.CONTENT_URI;
+
+      cr = context.getContentResolver();   
+      String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND (" 
+                              + Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                              + Calendars.OWNER_ACCOUNT + " = ?))";
+      String[] selectionArgs = new String[] {"sampleuser@gmail.com", "com.google",
+              "sampleuser@gmail.com"}; 
+      // Submit the query and get a Cursor object back. 
+      cursor = cr.query(uri, query, selection, selectionArgs, null);
+      
+      boolean done = cursor.moveToFirst();
+      boolean found = false;
+      while (!done) {
+    	  // Check if this record is the one we are trying to add
+    	  Integer id = cursor.getInt(PROJECTION_CALENDAR_ID_INDEX);
+    	  if (id == whichCalendar) { 
+    		  String title = cursor.getString(PROJECTION_TITLE_INDEX);
+    		  if (title.startsWith("Hockey- ")) {
+    			  String event = cursor.getString(PROJECTION_EVENT_LOCATION_INDEX);
+    			  if (event.startsWith("Twin Rinks Ice Arena - ")) {
+    				  Long start = cursor.getLong(PROJECTION_DTSTART_INDEX);
+    				  if ( start == game.getCal().getTimeInMillis() ) {
+    					  found = true;
+    		    	  }
+    			  }
+    		  }
+    		  if (found || !cursor.moveToNext())
+    			  done = true; 
+    	  }
+      }
+      if (!found) {
+	      // Check if the event already exists. If it does not add it.
+	      values.put(CalendarContract.Events.CALENDAR_ID, whichCalendar);
+	      values.put(CalendarContract.Events.TITLE, "Hockey- " + game.getLeague()
+	          + ": " + game.getTeamH() + " vs " + game.getTeamA());
+	      values.put(CalendarContract.Events.EVENT_LOCATION,
+	          "Twin Rinks Ice Arena - " + game.getRink() + " Rink");
+	      values.put(CalendarContract.Events.DTSTART, game.getCal()
+	          .getTimeInMillis());
+	      values.put(CalendarContract.Events.DTEND,
+	          game.getCal().getTimeInMillis() + 5400000);
+	      values.put(CalendarContract.Events.EVENT_TIMEZONE, 
+	    		  TimeZone.getTimeZone("America/Chicago")
+	          .getID());
+	      cr.insert(CalendarContract.Events.CONTENT_URI, values);
+      }
     }
     catch(Exception e) {
       util.err(e.getMessage());
